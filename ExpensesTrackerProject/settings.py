b6fab89bb01ea6a09fb77d3b89eca90617e66b0d
Marinestarter <shoelessmicah10@gmail.com>
2024-11-17 23:47:08 -0600

    working basic Create,Read, and Update Functionality, Delete not working yet.

--- ExpensesTrackerProject/settings.py
+++ ExpensesTrackerProject/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expense_app',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- expense_app/urls.py
+++ expense_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('efv', views.ExpenseFormView, name='new_expense_url'),
     path('sv/', views.showView, name='show_url'),
-    path('up/<int:f_id>', views.updateView, name= 'update_url'),
-    path('del/<int:f_id>', views.deleteView, name= 'delete_url'),
+    path('expense/', views.ExpenseFormView, name='new_expense_url'),
+    path('expense/<int:f_id>/', views.updateView, name='update_url'),
+    path('del/<int:f_id>', views.deleteView, name='delete_url'),
 ]
